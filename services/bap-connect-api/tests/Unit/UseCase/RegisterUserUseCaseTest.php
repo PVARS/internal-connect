b94b1f4cfd628d3f4e865fe70969387ce314043a
phanvuars <phanvuars@gmail.com>
2024-06-25 00:12:09 +0700

    feat: Update test case test_register_user_with_encrypt_token_fail

--- services/bap-connect-api/tests/Unit/UseCase/RegisterUserUseCaseTest.php
+++ services/bap-connect-api/tests/Unit/UseCase/RegisterUserUseCaseTest.php
@@ -4,6 +4,9 @@ namespace tests\Unit\UseCase;
 
 use App\Exceptions\AppException;
 use App\UseCase\RegisterUserUseCase;
+use App\Utils\Util;
+use Mockery;
+use Symfony\Component\HttpFoundation\Response as HttpCode;
 use Tests\TestCase;
 
 class RegisterUserUseCaseTest extends TestCase
@@ -16,14 +19,25 @@ class RegisterUserUseCaseTest extends TestCase
         $this->useCase = app(RegisterUserUseCase::class);
     }
 
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
     public function test_register_user_with_encrypt_token_fail(): void
     {
-        $_ENV['ENCRYPT_SECRET_KEY'] = '';
-        $_ENV['SECRET_IV'] = '';
+        $input = ['id' => fake()->uuid()];
+
+        $utilMock = Mockery::mock('alias:'.Util::class);
+        $utilMock->shouldReceive('opensslEncrypt')
+            ->with($input['id'])
+            ->andReturn(null);
 
         $this->expectException(AppException::class);
         $this->expectExceptionMessage('Failed to create verify user token');
+        $this->expectExceptionCode(HttpCode::HTTP_INTERNAL_SERVER_ERROR);
 
-        $this->useCase->run(['id' => fake()->uuid()]);
+        $this->useCase->run($input);
     }
 }
