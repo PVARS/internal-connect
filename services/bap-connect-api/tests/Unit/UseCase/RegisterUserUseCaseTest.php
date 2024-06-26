3ff88f4e5aaeab2555559501255a1a5992bd1f53
phanvars <phanvuars@gmail.com>
2024-06-26 14:57:53 +0700

    feat: Update tc encrypt token fail and add tc register user fail

--- services/bap-connect-api/tests/Unit/UseCase/RegisterUserUseCaseTest.php
+++ services/bap-connect-api/tests/Unit/UseCase/RegisterUserUseCaseTest.php
@@ -3,8 +3,9 @@
 namespace tests\Unit\UseCase;
 
 use App\Exceptions\AppException;
+use App\Repositories\UserRepositoryInterface;
 use App\UseCase\RegisterUserUseCase;
-use App\Utils\Util;
+use Exception;
 use Mockery;
 use Symfony\Component\HttpFoundation\Response as HttpCode;
 use Tests\TestCase;
@@ -17,25 +18,43 @@ class RegisterUserUseCaseTest extends TestCase
     {
         parent::setUp();
         $this->useCase = app(RegisterUserUseCase::class);
+
+        $_ENV['ENCRYPT_SECRET_KEY'] = 'my_secret_key';
+        $_ENV['SECRET_IV'] = 'my_secret_iv';
     }
 
     protected function tearDown(): void
     {
-        Mockery::close();
         parent::tearDown();
+        Mockery::close();
     }
 
     public function test_register_user_with_encrypt_token_fail(): void
     {
-        $input = ['id' => fake()->uuid()];
+        $_ENV['ENCRYPT_SECRET_KEY'] = '';
+        $_ENV['SECRET_IV'] = '';
 
-        $utilMock = Mockery::mock('alias:'.Util::class);
-        $utilMock->shouldReceive('opensslEncrypt')
-            ->with($input['id'])
-            ->andReturn(null);
+        $input = ['id' => fake()->uuid()];
 
         $this->expectException(AppException::class);
         $this->expectExceptionMessage('Failed to create verify user token');
+        $this->expectExceptionCode(HttpCode::HTTP_INTERNAL_SERVER_ERROR);
+
+        $this->useCase->run($input);
+    }
+
+    public function test_register_user_fail(): void
+    {
+        $input = ['id' => fake()->uuid()];
+
+        $mock = Mockery::mock(UserRepositoryInterface::class);
+        $mock
+            ->shouldReceive('create')
+            ->with($input)
+            ->andThrow(Exception::class, 'Register user failed', HttpCode::HTTP_INTERNAL_SERVER_ERROR);
+
+        $this->expectException(AppException::class);
+        $this->expectExceptionMessage('Register user failed');
         $this->expectExceptionCode(HttpCode::HTTP_INTERNAL_SERVER_ERROR);
 
         $this->useCase->run($input);
