d0d2afe5dd96c8169436137935f1ddc60e4e67a6
phanvuars <phanvuars@gmail.com>
2024-08-10 21:35:33 +0700

    feat: Update EmailRule

--- services/bap-connect-api/app/Rules/EmailRule.php
+++ services/bap-connect-api/app/Rules/EmailRule.php
@@ -2,6 +2,7 @@
 
 namespace App\Rules;
 
+use App\Utils\Constants;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
@@ -14,7 +15,7 @@ class EmailRule implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL) || strlen($value) > Constants::EMAIL_MAX_LENGTH) {
             $fail('The :attribute field must be a valid email address.');
         }
     }
